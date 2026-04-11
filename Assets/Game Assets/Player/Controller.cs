5910bec6de7ce3c41dc1441fab7aefe1307fbed5
ns-112 <nps007@latech.edu>
2026-04-10 19:15:47 -0500

    initial commit

---
+++ Assets/Game Assets/Player/Controller.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Controller : MonoBehaviour
+{
+    [SerializeField]
+    private InputActionAsset inputActions;
+
+    private InputAction left_right;
+    private InputAction up_down;
+
+
+
+    private Rigidbody2D rb;
+    private float moveSpeed = 5f;
+
+    void OnEnable()
+    {
+        var actionMap = inputActions.FindActionMap("Default");
+        left_right = actionMap.FindAction("LeftRight");
+        up_down = actionMap.FindAction("UpDown");
+
+        left_right.Enable();
+        up_down.Enable();
+
+        rb = GetComponent<Rigidbody2D>();
+
+
+    }
+
+   
+
+    void Update()
+    {
+        float x = left_right.ReadValue<float>();
+        float y = up_down.ReadValue<float>();
+
+        Debug.Log("x: " + x + " y: " + y);
+
+        rb.transform.position += new Vector3(x, y, rb.transform.position.z) * Time.deltaTime * moveSpeed;
+    }
+}
